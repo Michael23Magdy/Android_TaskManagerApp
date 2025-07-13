97fde9581e8de3b0fcc674dd8d76ae991ab68e64
Michael Magdy <michaelmagdymagdy@gmail.com>
2025-07-13 14:33:52 +0300

    removed nonused imports

--- app/src/main/java/com/example/android_taskmanagerapp/ui/components/OneTimeTaskComponent.kt
+++ app/src/main/java/com/example/android_taskmanagerapp/ui/components/OneTimeTaskComponent.kt
@@ -2,12 +2,9 @@ package com.example.android_taskmanagerapp.ui.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -25,13 +21,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.android_taskmanagerapp.model.OneTimeTask
 import com.example.android_taskmanagerapp.R
+import com.example.android_taskmanagerapp.model.OneTimeTask
 
 @Composable
 fun OneTimeTaskComponent(
     task: OneTimeTask,
-    onDoneChange: () -> Unit = {},
+    onDoneChange: () -> Unit,
     modifier: Modifier = Modifier
 ){
     var expanded by rememberSaveable { mutableStateOf(false) }
@@ -100,7 +96,8 @@ fun OneTimeTaskPreview(){
     LazyColumn {
         items(10){
             OneTimeTaskComponent(
-                OneTimeTask("HW", "finish 3 pages")
+                OneTimeTask("HW", "finish 3 pages"),
+                {}
             )
         }
     }
