8d92b32e3eb41d3f8c72f515285934435ee992e2
Michael Magdy <michaelmagdymagdy@gmail.com>
2025-07-14 02:23:45 +0300

    added no tasks screen look

--- app/src/main/java/com/example/android_taskmanagerapp/ui/MyTasksScreen.kt
+++ app/src/main/java/com/example/android_taskmanagerapp/ui/MyTasksScreen.kt
@@ -2,12 +2,15 @@ package com.example.android_taskmanagerapp.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -28,7 +31,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.android_taskmanagerapp.R
 import com.example.android_taskmanagerapp.model.AbstractTask
@@ -77,9 +82,24 @@ fun MyTasksScreen(
             }
             if(listUiState.tasks.isEmpty()){
                 item {
-                    Text(
-                        text = "No tasks yet",
-                    )
+                    Column(
+                        modifier = modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Spacer(modifier = Modifier.height(15.dp))
+                        Icon(
+                            painter = painterResource(R.drawable.outline_bedtime_24),
+                            contentDescription = null,
+                            modifier = Modifier.size(150.dp),
+                            tint = Color.LightGray
+                        )
+                        Text(
+                            text = "No tasks yet",
+                            fontSize = 30.sp,
+                            color = Color.LightGray,
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
             } else {
                 items(listUiState.tasks) { task ->
