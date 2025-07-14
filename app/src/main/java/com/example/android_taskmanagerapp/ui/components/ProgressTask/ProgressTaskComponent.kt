5d667bf3b1159dbf3f10adea26e914262242fcfb
Michael Magdy <michaelmagdymagdy@gmail.com>
2025-07-14 12:03:18 +0300

    added done look for progress tasks

--- app/src/main/java/com/example/android_taskmanagerapp/ui/components/ProgressTask/ProgressTaskComponent.kt
+++ app/src/main/java/com/example/android_taskmanagerapp/ui/components/ProgressTask/ProgressTaskComponent.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android_taskmanagerapp.model.ProgressTask
@@ -63,7 +65,12 @@ fun ProgressTaskComponent(
             ) {
                 Text(
                     text = task.title,
-                    style = MaterialTheme.typography.headlineSmall,
+                    style = MaterialTheme.typography.headlineSmall.merge(
+                        TextStyle(
+                            textDecoration = if (task.isDone()) TextDecoration.LineThrough else TextDecoration.None,
+                            color = if (task.isDone()) Color.Gray else Color.Black
+                        )
+                    )
                 )
                 Text(
                     text = "you have finished ${task.progressValue} out of ${task.numSubtasks}",
